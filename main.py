a6c54b9dd6de3b2c55028441cc9e6663966e95a6
unknown <pandupurwadi18@gmail.com>
2022-06-12 17:56:32 +0700

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+# --- Football Team's Name Generator ---
+# 100 days of code Python
+# Pandu Purwadi 
+
+# 1. Greeting for user
+print("Welcome to the Football Name Generator.\nYour future team will be born!")
+# 2. Ask the user for location they live
+location_name = input("What's name of the city/town you live?\n")
+# 3. Ask the user for their favorite color
+color_fav = input("What is your favorite color?\n")
+# 4. Combine two variables above for their football name
+print("Your football team's name could be"+" "+color_fav+" "+location_name+" " +"Football Club")
+
